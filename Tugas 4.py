78b4374c58ef3184de711b833cf6550313294b18
Atreus883 <miftahrijallul88@gmail.com>
2025-04-20 19:02:22 +0700

    mengumpulkan

--- Tugas 4.py
+++ Tugas 4.py
@@ -13,7 +13,7 @@ import urllib.request
 import zipfile
 import tensorflow as tf
 import os
-from keras_preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop
 
 
@@ -45,13 +45,45 @@ def solution_05():
     
     # YOUR CODE HERE
 
+    VALIDATION_DIR = 'data/validation-horse-or-human'
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+    validation_generator = validation_datagen.flow_from_directory(
+        VALIDATION_DIR,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary'
+    )
+
+
     model=tf.keras.models.Sequential([
         # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
+                tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+                tf.keras.layers.MaxPooling2D(2, 2),
 
+                tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+                tf.keras.layers.MaxPooling2D(2, 2),
+
+                tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                tf.keras.layers.MaxPooling2D(2, 2),
+
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(512, activation='relu'),
+                tf.keras.layers.Dropout(0.5),
                 tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
         ])
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer=RMSprop(learning_rate=0.001),
+        metrics=['accuracy']
+    )
 
-    return model
+    return model, train_generator, validation_generator
 
 
 # The code below is to save your model as a .h5 file.
